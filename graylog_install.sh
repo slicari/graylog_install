f481373729bc175c79c83060be123c88fb60af1f
slicari <steve_licari@yahoo.com>
2018-03-05 13:35:52 -0500

    Updated graylog install script to include docker-compose

--- graylog_install.sh
+++ graylog_install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #
+#
 # Updates boiii
 apt-get update
 apt-get upgrade
@@ -11,4 +12,8 @@ mkdir -p ./graylog/config
 cd ./graylog/config
 wget https://raw.githubusercontent.com/Graylog2/graylog-docker/2.4/config/graylog.conf
 wget https://raw.githubusercontent.com/Graylog2/graylog-docker/2.4/config/log4j2.xml
-
+# Install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+# Confirm docker-compose installed
+docker-compose --version
